1de4e69d00b04ea47ebd86d09a5ceb4e7f9f2b40
Daniil Pavlov <Dan43575@gmail.com>
2022-01-24 01:55:04 +0300

    Finished layout structure, added styling for desktop version, changed up folder structre

--- src/components/comment-thread/comment-thread.tsx
+++ src/components/comment-thread/comment-thread.tsx
@@ -1,5 +1,47 @@
-import React, { Component, FC } from "react";
+import React, { FC } from "react";
 
-const CommentThread: FC = (props) => {
-  return <div></div>;
+import { Comment } from "../../types/Comment";
+import CommentBox from "../comment-box/comment-box";
+import "./comment-thread.css";
+
+interface ICommentSectionProps {
+  comment: Comment;
+}
+
+const CommentThread: FC<ICommentSectionProps> = ({ comment }) => {
+  return (
+    <div className="comment-thread">
+      <CommentBox
+        key={comment.id}
+        content={comment.content}
+        createdAt={comment.createdAt}
+        score={comment.score}
+        image={comment.user.image}
+        username={comment.user.username}
+      />
+      {comment.replies.length ? (
+        <div className="comment-replies">
+          <div className="thread-line"></div>
+          <div className="comment-replies-boxes">
+            {comment.replies.map((reply) => {
+              return (
+                <CommentBox
+                  key={reply.id}
+                  content={reply.content}
+                  createdAt={reply.createdAt}
+                  score={reply.score}
+                  image={reply.user.image}
+                  username={reply.user.username}
+                />
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
+
+export default CommentThread;
